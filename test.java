92f4bc45198a9edcce4f037cce6d9d3d4690a3a7
MyshkevichAV <147534419+MyshkevichAV@users.noreply.github.com>
2023-10-12 18:35:44 +0300

    add test

---
+++ test.java
@@ -0,0 +1,10 @@
+public class World {
+    public static void main(String[] args) {
+        double z = multiply(10, 20, 30);
+        System.out.println(z);
+    }
+
+    public static int multiply(int a, int b, int c) {
+        return a * b * c;
+    }
+}
